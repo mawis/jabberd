7056bb29c768876143ab4e90cb609e030d09759a
jeremie <jeremie@localhost>
2000-08-06 08:13:05 +0000

    we now have nice xdb request handling for loaded modules

--- jabberd/Attic/io/base_load.c
+++ jabberd/Attic/io/base_load.c
@@ -59,20 +59,35 @@ void *base_load_symbol(char *func, char *file)
 result base_load_config(instance id, xmlnode x, void *arg)
 {
     xmlnode so;
+    char *init = xmlnode_get_attrib(x,"main");
+    void *f;
+    int flag = 0;
 
-    if(id == NULL)
-    {
-        printf("base_load_config validating configuration\n");
+    if(init == NULL) return r_ERR;
+
+    if(id != NULL)
+    { /* execution phase */
+        f = xmlnode_get_vattrib(x, init);
+        ((base_load_init)f)(id, x); /* fire up the main function for this extension */
         return r_PASS;
     }
 
-    printf("base_load_config performing configuration %s\n",xmlnode2str(x));
-    /* scan through the dso's specified */
-    for(so = xmlnode_get_firstchild(x); so != NULL; so = xmlnode_get_nextsibling(so))
-        if(xmlnode_get_type(so) == NTYPE_TAG)
-            /*loader_dso(xmlnode_get_data(mod), xmlnode_get_name(mod))*/;
+    printf("base_load_config processing configuration %s\n",xmlnode2str(x));
 
-    /* with each load section, register a handler for the xdb blocked ring */
+    for(so = xmlnode_get_firstchild(x); so != NULL; so = xmlnode_get_nextsibling(so))
+    {
+        if(xmlnode_get_type(so) != NTYPE_TAG) continue;
+
+        f = base_load_symbol(xmlnode_get_name(so), xmlnode_get_data(so));
+        if(f == NULL)
+            return r_ERR;
+        xmlnode_put_vattrib(x, xmlnode_get_name(so), f); /* hide the function pointer in the <load> element for later use */
+        flag = 1;
+    }
+
+    if(!flag) return r_ERR; /* we didn't DO anything, duh */
+
+    return r_PASS;
 }
 
 void base_load(void)
@@ -96,18 +111,183 @@ some sort of timer that scans the ring oldest to newest, resending stale request
 start sending logs depending on length of staleness
 */
 
-/* blocks until namespace is retrieved, host must map back to this service! */
-xmlnode xdb_get(xdbcache xc, char *host, jid to, char *ns)
+result xdb_results(instance id, dpacket p, void *arg)
 {
-    /* create the xml and deliver the xdb get request */
-    /* insert an entry in this instances xdb blocked ring */
-    /* if get back type="error" log that */
-    /* returns the xmlnode inside <xdb>...</xdb> if success, or NULL if fail */
+    xdbcache xc = (xdbcache)arg;
+    xdbcache curx;
+    int idnum;
+    char *idstr;
+
+    if(strcmp(xmlnode_get_name(p->x),"xdb") != 0) return r_PASS;
+
+    if((idstr = xmlnode_get_attrib(p->x,"id")) == NULL) return r_ERR;
+
+    idnum = atoi(idstr);
+
+    for(curx = xc->next; curx->id != idnum && curx != xc; curx = curx->next); /* spin till we are where we started or find our id# */
+
+    if(curx->id != idnum) return r_OK; /* we got an id we didn't have cached, could be a dup, ignore and move on */
+
+    /* associte packet w/ waiting cache */
+    curx->data = p->x;
+
+    /* remove from ring */
+    curx->prev->next = curx->next;
+    curx->next->prev = curx->prev;
+
+    /* free the thread! */
+    curx->preblock = 0;
+    if(curx->cond != NULL)
+        pth_cond_notify(curx->cond, FALSE);
+
+    return r_OK; /* we processed it */
 }
 
-/* sends new xml to replace old */
-int xdb_set(xdbcache xc, char *host, jid to, xmlnode data)
+xdbcache xdb_cache(instance id)
 {
-    /* blocks just like the get, returns non-zero if error */
-    /* if get back type="error" log that */
+    xdbcache newx;
+
+    if(id == NULL)
+    {
+        fprintf(stderr,"Programming Error: xdb_cache() called with NULL\n");
+        return NULL;
+    }
+
+    newx = pmalloco(id->p, sizeof(_xdbcache));
+    newx->i = id; /* flags it as the top of the ring too */
+    newx->next = newx->prev = newx; /* init ring */
+
+    /* register the handler in the instance to filter out xdb results */
+    register_phandler(id, o_FIRST, xdb_results, (void *)newx);
+
+    return newx;
+}
+
+/* blocks until namespace is retrieved, host must map back to this service! */
+xmlnode xdb_get(xdbcache xc, char *host, jid owner, char *ns)
+{
+    _xdbcache newx;
+    xmlnode x;
+    char ids[9];
+    pth_mutex_t mutex = PTH_MUTEX_INIT;
+    pth_cond_t cond = PTH_COND_INIT;
+
+    if(xc == NULL || host == NULL || owner == NULL || ns == NULL)
+    {
+        fprintf(stderr,"Programming Error: xdb_get() called with NULL\n");
+        return NULL;
+    }
+
+    /* init this newx */
+    newx.i = NULL;
+    newx.data = NULL;
+    newx.host = host;
+    newx.ns = ns;
+    newx.owner = owner;
+    newx.sent = time(NULL);
+    newx.preblock = 1; /* flag */
+    newx.cond = NULL;
+
+    /* in the future w/ real threads, would need to lock xc to make these changes to the ring */
+    newx.id = xc->id++;
+    newx.next = xc->next;
+    newx.prev = xc;
+    newx.next->prev = &newx;
+    xc->next = &newx;
+
+    /* create the xml and deliver the xdb get request */
+    x = xmlnode_new_tag("xdb");
+    xmlnode_put_attrib(x,"type","get");
+    xmlnode_put_attrib(x,"to",jid_full(owner));
+    xmlnode_put_attrib(x,"from",host);
+    sprintf(ids,"%d",newx.id);
+    xmlnode_put_attrib(x,"id",ids); /* to track response */
+    deliver(dpacket_new(x));
+
+    /* if it hasn't already returned, we should block here until it returns */
+    if(newx.preblock)
+    {
+        newx.cond = &cond;
+        pth_mutex_acquire(&mutex, FALSE, NULL);
+        pth_cond_await(&cond, &mutex, NULL); /* blocks thread */
+    }
+
+    /* newx.data is now the returned xml packet */
+
+    /* if get back type="error" log that and return NULL */
+    if(j_strcmp(xmlnode_get_attrib(newx.data, "type"),"error") == 0)
+    {
+        log_notice(host,"xdb_get failed for %s to %s",ns,jid_full(owner));
+        xmlnode_free(newx.data);
+        return NULL;
+    }
+
+    /* return the xmlnode inside <xdb>...</xdb> */
+    for(x = xmlnode_get_firstchild(newx.data); x != NULL && xmlnode_get_type(x) != NTYPE_TAG; x = xmlnode_get_nextsibling(x));
+
+    return x;
+}
+
+/* sends new xml to replace old, data is NOT freed, app responsible for freeing it */
+int xdb_set(xdbcache xc, char *host, jid owner, xmlnode data)
+{
+    _xdbcache newx;
+    xmlnode x;
+    char ids[9];
+    pth_mutex_t mutex = PTH_MUTEX_INIT;
+    pth_cond_t cond = PTH_COND_INIT;
+
+    if(xc == NULL || host == NULL || owner == NULL || data == NULL)
+    {
+        fprintf(stderr,"Programming Error: xdb_set() called with NULL\n");
+        return 1;
+    }
+
+    /* init this newx */
+    newx.i = NULL;
+    newx.data = data;
+    newx.host = host;
+    newx.ns = NULL;
+    newx.owner = owner;
+    newx.sent = time(NULL);
+    newx.preblock = 1; /* flag */
+    newx.cond = NULL;
+
+    /* in the future w/ real threads, would need to lock xc to make these changes to the ring */
+    newx.id = xc->id++;
+    newx.next = xc->next;
+    newx.prev = xc;
+    newx.next->prev = &newx;
+    xc->next = &newx;
+
+    /* create the xml and deliver the xdb get request */
+    x = xmlnode_new_tag("xdb");
+    xmlnode_put_attrib(x,"type","set");
+    xmlnode_put_attrib(x,"to",jid_full(owner));
+    xmlnode_put_attrib(x,"from",host);
+    sprintf(ids,"%d",newx.id);
+    xmlnode_put_attrib(x,"id",ids); /* to track response */
+    xmlnode_insert_tag_node(x,data); /* copy in the data */
+    deliver(dpacket_new(x));
+
+    /* if it hasn't already returned, we should block here until it returns */
+    if(newx.preblock)
+    {
+        newx.cond = &cond;
+        pth_mutex_acquire(&mutex, FALSE, NULL);
+        pth_cond_await(&cond, &mutex, NULL); /* blocks thread */
+    }
+
+    /* newx.data is now the returned xml packet */
+
+    /* if get back type="error" log that and return NULL */
+    if(j_strcmp(xmlnode_get_attrib(newx.data, "type"),"error") == 0)
+    {
+        for(x = xmlnode_get_firstchild(newx.data); x != NULL && xmlnode_get_type(x) != NTYPE_TAG; x = xmlnode_get_nextsibling(x));
+        log_notice(host,"xdb_set failed for %s to %s",xmlnode_get_attrib(x, "xmlns"),jid_full(owner));
+        xmlnode_free(newx.data);
+        return 1;
+    }
+
+    return 0;
 }
