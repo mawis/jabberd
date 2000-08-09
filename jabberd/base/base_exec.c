73e1ed14c86cbb4b878ddf7a099788729284af6f
tsbandit <tsbandit@localhost>
2000-08-09 04:51:39 +0000

    here's an idea for the exec base module.. a start of one anyway.. someone fix this so that it works right..

--- jabberd/base/base_exec.c
+++ jabberd/base/base_exec.c
@@ -3,6 +3,25 @@
 /* consult apache source to figure out how to do this */
 /* fork/exec a command, serialize xmlnodes to its STDIN for incoming packets, read it's STDOUT as an xmlstream */
 
+/* something along the lines of:
+   int pid;
+   int status;
+   if((pid=fork())<0)
+      log_error(ZONE,"failed to fork");
+   else if(pid==0)
+   { // child process
+     execlp(filename,filename,(char*)0);
+     exit(127);
+   }
+   // parent
+   if((pid=waitpid(pid,&status,0))<0)
+     log_error(ZONE,"wait_pid err??");
+   if(status==127)
+     log_error(ZONE,"child process died, restart it");
+
+     but that will make the whole jabberd block on the child process won't it?  *shrug* well, that's just a start, I don't *really* know what I'm doing.. =]
+    */ 
+
 result base_exec_config(instance id, xmlnode x, void *arg)
 {
     if(id == NULL)
