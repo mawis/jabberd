e97410cf09f7223583d2e00e746292656cbbfdba
jeremie <jeremie@localhost>
2000-08-22 07:52:26 +0000

    fleshing out accept logic more

--- jabberd/Attic/io/base_accept.c
+++ jabberd/Attic/io/base_accept.c
@@ -17,6 +17,13 @@ typedef struct sink_struct
     void *foo;
 } *sink, _sink;
 
+typedef struct acceptor_struct
+{
+    int sock, flag_ok, flag_read, flag_write;
+    pth_cond_t cond_secret;
+    instance i;
+} *acceptor, _acceptor;
+
 void base_accept_phandler(instance i, dpacket p, sink s)
 {
     /* write packets to sink */
@@ -24,47 +31,103 @@ void base_accept_phandler(instance i, dpacket p, sink s)
 
 void *base_accept_write(void *arg)
 {
-    /* first, block on a condition from the read thread, then write the header and block on the sink */
-    /* second, block on a condition from the read thread for success/failure of secret negotiation, write <secret/> or stream:error & close */
-    /* if the write() fails, return the packet to the sink */
+    /* write the header */
+    /* block on a condition from the read thread for success/failure of secret negotiation, write <secret/> or stream:error & close */
+    /* then block on the sink */
+    /* if the write() fails, return the packet to the sink and close */
 }
 
-void base_accept_read_packets(xstream xs)
+void base_accept_read_packets(xstream xs, void *arg)
 {
-    /* after getting a root, send header */
-    /* check status on socket, if it's sent a secret, then deliver the packet, otherwise only validate a secret */
+    acceptor a = (acceptor)arg;
+
+    /* after getting a root, spawn write thread */
+    pth_spawn(PTH_ATTR_DEFAULT,base_accept_write,arg)
+
+    /* check status on socket, if it's sent a secret, then deliver the packet */
+    if(a->flag_ok)
+    {
+        /* deliver */
+        return;
+    }
+
+    /* check the secret */
     /* based on secret, store instance this socket is associated with */
-    /* need the ring of secrets for validation from the parent listen thread */
+
 }
 
+/* thread to read from socket */
 void *base_accept_read(void *arg)
 {
-    /* thread to read from socket */
+    acceptor a = (acceptor)arg;
+
     /* create/deliver to xstream */
-    /* spawn base_accept_write */
     /* if error on read(), just cleanup and quit */
 }
 
+/* thread to listen on a particular port/ip */
+void *base_accept_listen(void *arg)
+{
+    acceptor a;
+
+    /* look at the port="" and optional ip="" attribs and start listening */
+
+    /* when we get a new socket */
+    /* create acceptor */
+    pth_spwan(PTH_ATTR_DEFAULT, base_accept_read, (void *)a);
+}
+
+xmlnode base_accept__listeners;
 
 result base_accept_config(instance id, xmlnode x, void *arg)
 {
+    char *port, *ip;
+    xmlnode cur;
+    sink s;
+
+    port = xmlnode_get_data(xmlnode_get_tag(x, "port"));
+    ip = xmlnode_get_data(xmlnode_get_tag(x, "ip"));
     if(id == NULL)
     {
         printf("base_accept_config validating configuration\n");
-	if(xmlnode_get_data(xmlnode_get_tag(x, "port")) == NULL || xmlnode_get_data(xmlnode_get_tag(x, "secret")) == NULL)
+	if(port == NULL || xmlnode_get_data(xmlnode_get_tag(x, "secret")) == NULL)
 	    return r_ERR;
         return r_PASS;
     }
 
     printf("base_accept_config performing configuration %s\n",xmlnode2str(x));
-    /* find existing listen thread (or create) */
-    /* add secret+instance to listen thread ring */
+
+    /* look for an existing accept section that is the same */
+    for(cur = xmlnode_get_firstchild(base_accept__listeners); cur != NULL; cur = xmlnode_get_nextsibling(cur))
+        if(strcmp(port,xmlnode_get_attrib(cur,"port")) == 0 && (ip == NULL && xmlnode_get_attrib(cur,"ip") == NULL || strcmp(ip,xmlnode_get_attrib(cur,"ip")) == 0))
+            break;
+
+    /* create a new section for this section */
+    if(cur == NULL)
+    {
+        cur = xmlnode_insert_tag(base_accept__listeners, "listen");
+        xmlnode_put_attrib(cur,"port",port);
+        xmlnode_put_attrib(cur,"ip",ip);
+
+        /* start a new listen thread */
+        pth_spawn(PTH_ATTR_DEFAULT, base_accept_listen, (void *)cur);
+    }
+
+    /* insert secret into it and hide instance in that new secret */
+    xmlnode_put_vattrib(xmlnode_insert_tag_node(cur,xmlnode_get_tag(x,"secret")),"i",(void *)id);
+
     /* create sink and register phandler, and register cleanup heartbeat */
+    s = pmalloc_x(id->p, sizeof(_sink));
+
+    /* hide sink as an attrib on the section */
 }
 
 void base_accept(void)
 {
     printf("base_accept loading...\n");
+
+    /* master list of all listen threads */
+    base_accept__listeners = xmlnode_new_tag("listeners");
 
     register_config("accept",base_accept_config,NULL);
 }
