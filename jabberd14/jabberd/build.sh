0131ac24a3567fe456feba8a27b4284a258f9dc2
tsbandit <tsbandit@localhost>
2000-08-30 09:51:48 +0000

    lala

--- jabberd14/jabberd/build.sh
+++ jabberd14/jabberd/build.sh
@@ -1,2 +1,2 @@
-gcc -c *.c */*.c -DCONFIGXML="\"./jabberd.xml\"" -I. -I/opt/jabber-1.0/include
-gcc -o jabberd *.o -lpth -ljabber -lxode -ldl -L/opt/jabber-1.0/lib
+gcc -g -c *.c */*.c -DCONFIGXML="\"./jabberd.xml\"" -I. -I/opt/jabber-1.0/include
+gcc -g -o jabberd *.o -lpth -ljabber -lxode -ldl -L/opt/jabber-1.0/lib
