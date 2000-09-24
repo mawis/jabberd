42887ba07933236f7325db5c491ba80ce1e4c045
tsbandit <tsbandit@localhost>
2000-09-24 07:09:38 +0000

    new io_select model

---
+++ jabberd14/pthsock/client.c
@@ -0,0 +1,343 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jabber
+ *  Copyright (C) 1998-1999 The Jabber Team http://jabber.org/
+ */
+
+/*
+    <service id="pthsock client">
+      <host>pth-csock.127.0.0.1</host>
+      <load>
+	    <pthsock_client>../load/pthsock_client.so</pthsock_client>
+      </load>
+      <pthcsock xmlns='jabberd:pth-csock:config'>
+	    <host>pth-csock.127.0.0.1</host>
+        <listen>5222</listen>
+      </pthcsock>
+    </service>
+*/
+
+#include "io.h"
+
+/* socket manager instance */
+typedef struct smi_st
+{
+    instance i;
+    sock conns;
+    xmlnode cfg;
+    pth_msgport_t wmp;
+    char *host;
+    int asock;  /* socket we accept connections on */
+} *smi, _smi;
+
+typedef struct cdata_st
+{
+    smi i;
+    int state;
+    char *id, *host, *sid, *res, *auth_id;
+    void *arg;
+} _cdata,*cdata;
+
+void pthsock_client_unlink(smi si, sock c)
+{
+    sock cur, prev;
+
+    log_debug(ZONE,"Unlinking Local sock copy %d",c->fd);
+
+    /* remove connection from the list */
+    for (cur = si->conns,prev = NULL; cur != NULL; prev = cur,cur = cur->next)
+        if (cur == c)
+        {
+            if (prev != NULL)
+                prev->next = cur->next;
+            else
+                si->conns = cur->next;
+            break;
+        }
+}
+
+void pthsock_client_close(sock c)
+{
+    xmlnode x;
+    cdata cd=(cdata)c->arg;
+    smi si=cd->i;
+
+    log_debug(ZONE,"asking %d to close",c->fd);
+    io_close(c);
+    pthsock_client_unlink(si,c);
+
+    log_debug(ZONE,"send 510 to session manager");
+    x = xmlnode_new_tag("message");
+    jutil_error(x,TERROR_DISCONNECTED);
+    xmlnode_put_attrib(x,"sto",cd->host);
+    xmlnode_put_attrib(x,"sfrom",cd->id);
+    deliver(dpacket_new(x),cd->i->i);
+}
+
+result pthsock_client_packets(instance id, dpacket p, void *arg)
+{
+    smi si=(smi)arg;
+    sock cur;
+    cdata cd;
+    char *type;
+    int sock;
+
+    log_debug(ZONE,"Got a packet from jabberd: %s",xmlnode2str(p->x));
+
+    if (p->id->user == NULL)
+    {
+        log_debug(ZONE,"not a user %s",xmlnode2str(p->x));
+        xmlnode_free(p->x);
+        return r_DONE;
+    }
+
+    sock = atoi(p->id->user); 
+    if (sock == 0)
+    {
+        xmlnode_free(p->x);
+        return r_DONE;
+    }
+
+    log_debug(ZONE,"pthsock_client looking up %d",sock);
+
+    /* XXX this for loop is UUUU-GLY */
+    for (cur = si->conns; cur != NULL; cur = cur->next)
+    {if (sock == cur->fd)
+     {
+      cd=(cdata)cur->arg;
+      if (j_strcmp(p->id->resource,cd->res) == 0)
+      { /* check to see if the session manager killed the session */
+       if (*(xmlnode_get_name(p->x)) == 'm')
+       {
+        if (xmlnode_get_tag(p->x,"error?code=510")!=NULL)
+        {
+         log_debug(ZONE,"received disconnect message from session manager");
+         io_write_str(cur,"<stream:error>Disconnected</stream:error>");
+         pthsock_client_close(cur);
+         return -1;
+        }
+       }
+       if(cd->state==state_UNKNOWN&&*(xmlnode_get_name(p->x))=='i')
+       {if(j_strcmp(xmlnode_get_attrib(p->x,"type"),"result") == 0)
+        {if (j_strcmp(cd->auth_id,xmlnode_get_attrib(p->x,"id")) == 0)
+         {
+          log_debug(ZONE,"auth for %d successful",cd->id);
+          /* change the host id */
+          cd->host = pstrdup(cur->p,xmlnode_get_attrib(p->x,"sfrom"));
+          cur->state = state_AUTHD;
+         }
+         else log_debug(ZONE,"reg for %d successful",cur->fd);
+        }
+        else log_debug(ZONE,"user auth/registration falid");
+       }
+       xmlnode_hide_attrib(p->x,"sto");
+       xmlnode_hide_attrib(p->x,"sfrom");
+       io_write(cur,p->x);
+       pool_free(p->p);
+
+      }
+      else break;
+      return r_DONE;
+     }
+    }
+
+    /* don't bounce if it's error 510 */
+    if (*(xmlnode_get_name(p->x)) == 'm')
+        if (j_strcmp(xmlnode_get_attrib(p->x,"type"),"error") == 0)
+            if (j_strcmp(xmlnode_get_attrib(xmlnode_get_tag(p->x,"error"),"code"),"510") == 0)
+            {
+                xmlnode_free(p->x);
+                return r_DONE;
+            }
+
+    log_debug(ZONE,"pthsock_client connection not found");
+
+    xmlnode_put_attrib(p->x,"sto",xmlnode_get_attrib(p->x,"sfrom"));
+    xmlnode_put_attrib(p->x,"sfrom",jid_full(p->id));
+    type = xmlnode_get_attrib(p->x,"type");
+
+    jutil_error(p->x,TERROR_DISCONNECTED);
+
+    if (type != NULL)
+        /* HACK: hide the old type on the 510 error node */
+        xmlnode_put_attrib(xmlnode_get_tag(p->x,"error?code=510"),"type",type);
+
+    jutil_tofrom(p->x);
+    deliver(dpacket_new(p->x),si->i);
+
+    return r_DONE;
+}
+
+/* callback for xstream */
+void pthsock_client_stream(int type, xmlnode x, void *arg)
+{
+    sock c = (sock)arg;
+    cdata cs=(cdata)c->arg;
+    xmlnode h;
+    char *block;
+
+    log_debug(ZONE,"Got Type %d packet from io_select: %s",type,xmlnode2str(x));
+
+    switch(type)
+    {
+    case XSTREAM_ROOT:
+        log_debug(ZONE,"root received for %d",c->fd);
+
+        /* write are stream header */
+        cs->host = pstrdup(c->p,xmlnode_get_attrib(x,"to"));
+        h = xstream_header("jabber:client",NULL,cs->host);
+        cs->sid = pstrdup(c->p,xmlnode_get_attrib(h,"id"));
+        block = xstream_header_char(h);
+        io_write_str(c,block);
+        xmlnode_free(h);
+        xmlnode_free(x);
+        break;
+
+    case XSTREAM_NODE:
+        log_debug(ZONE,">>>> %s",xmlnode2str(x));
+
+        /* only allow auth and registration queries at this point */
+        if (c->state == state_UNKNOWN)
+        {
+            xmlnode q = xmlnode_get_tag(x,"query");
+            if (*(xmlnode_get_name(x)) != 'i' || (NSCHECK(q,NS_AUTH) == 0 && NSCHECK(q,NS_REGISTER) == 0))
+            {
+                log_debug(ZONE,"user tried to send packet in unknown state");
+                /* bounce */
+                xmlnode_free(x);
+                c->state=state_CLOSE;
+                return;
+            }
+            else if (NSCHECK(q,NS_AUTH))
+            {
+                xmlnode_put_attrib(xmlnode_get_tag(q,"digest"),"sid",cs->sid);
+                cs->auth_id = pstrdup(c->p,xmlnode_get_attrib(x,"id"));
+                if (cs->auth_id == NULL) /* if they didn't supply an id, then we make one */
+                {
+                    cs->auth_id = pstrdup(c->p,"1234");
+                    xmlnode_put_attrib(x,"id","1234");
+                }
+            }
+        }
+
+        xmlnode_put_attrib(x,"sfrom",cs->id);
+        xmlnode_put_attrib(x,"sto",cs->host);
+        deliver(dpacket_new(x),cs->i->i);
+        break;
+
+    case XSTREAM_ERR:
+        io_write_str(c,"<stream:error>You sent malformed XML</stream:error>");
+    case XSTREAM_CLOSE:
+        log_debug(ZONE,"closing XSTREAM");
+        pthsock_client_close(c);
+        xmlnode_free(x);
+    }
+}
+
+
+cdata pthsock_client_cdata(smi si,sock c)
+{
+    cdata cd;
+    char buf[100];
+
+    log_debug(ZONE,"Creating a new cdata to match socket %d",c->fd);
+    cd = pmalloco(c->p, sizeof(_cdata));
+    cd->i = si;
+    c->xs = xstream_new(c->p,pthsock_client_stream,(void*)c);
+    c->state = state_UNKNOWN;
+
+    memset(buf,0,99);
+
+    /* HACK to fix race conditon */
+    snprintf(buf,99,"%d",&c);
+    cd->res = pstrdup(c->p,buf);
+
+    /* we use <fd>@host to identify connetions */
+    snprintf(buf,99,"%d@%s/%s",c->fd,si->host,cd->res);
+    cd->id = pstrdup(c->p,buf);
+
+    log_debug(ZONE,"socket id:%s",cd->id);
+
+    return cd;
+}
+
+void pthsock_client_read(sock c,char *buffer,int bufsz,int flags,void *arg)
+{
+    smi si=(smi)arg;
+    cdata cd;
+
+    if(c!=NULL) 
+    {
+     log_debug(ZONE,"io_select read event on %d:%d:%d, [%s]",c->fd,flags,bufsz,buffer);
+    }
+    else
+    {
+     log_debug(ZONE,"io_select read event [%s]:%d:%d",buffer,bufsz,flags);
+    }
+
+    switch(flags)
+    {
+    case IO_INIT:
+        log_debug(ZONE,"io_select INIT event");
+        break;
+    case IO_NEW:
+        log_debug(ZONE,"io_select NEW socket connected at %d",c->fd);
+        cd=pthsock_client_cdata(si,c);
+        c->arg=(void*)cd;
+        break;
+    case IO_NORMAL:
+        log_debug(ZONE,"io_select NORMAL data");
+        xstream_eat(c->xs,buffer,bufsz);
+        break;
+    case IO_CLOSED:
+        log_debug(ZONE,"io_select Socket %d close notification",c->fd);
+        pthsock_client_unlink(si,c);
+        break;
+    }
+}
+
+/* everything starts here */
+void pthsock_client(instance i, xmlnode x)
+{
+    smi si;
+    xdbcache xc;
+    char *host, *port;
+
+    log_debug(ZONE,"pthsock_client loading");
+
+    si = pmalloco(i->p,sizeof(_smi));
+
+    /* write mp */
+    si->wmp = pth_msgport_create("pthsock_client_wmp");
+
+    /* get the config */
+    xc = xdb_cache(i);
+    si->cfg = xdb_get(xc,NULL,jid_new(xmlnode_pool(x),"config@-internal"),"jabberd:pth-csock:config");
+
+    si->host = host = xmlnode_get_tag_data(si->cfg,"host");
+    port = xmlnode_get_tag_data(si->cfg,"listen");
+
+
+    if (host == NULL || port == NULL)
+    {
+        log_error(ZONE,"pthsock_client invaild config");
+        return;
+    }
+
+    /* register data callbacks */
+    io_select(atoi(port),pthsock_client_read,(void*)si);
+    register_phandler(i,o_DELIVER,pthsock_client_packets,(void*)si);
+}
---
+++ jabberd14/pthsock/io_select.c
@@ -0,0 +1,423 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jabber
+ *  Copyright (C) 1998-1999 The Jabber Team http://jabber.org/
+ */
+
+#include "io.h"
+
+pth_mutex_t m_sync=PTH_MUTEX_INIT;
+
+typedef struct io_st
+{
+    pool p;
+    int master_fd;
+    sock master__list;
+    io_cb cb;
+    void *cb_arg;
+    void *arg;
+    pth_msgport_t wmp;
+} _ios,*ios;
+
+
+int _io_write_dump(sock c)
+{
+    int len,retval;
+
+    if(c->wbuffer==NULL) return 0;
+
+    len=write(c->fd,c->wbuffer,strlen(c->wbuffer));
+    log_debug(ZONE,"dumped %d bytes of %d",len,strlen(c->wbuffer));
+    if(len==0)
+    {
+        /* we didn't write anything.. postpone the write */
+        retval=1;
+    }
+    if(len<0)
+    { /* error occured while writing the packet */
+        if(errno!=EWOULDBLOCK)
+        {
+            free(c->wbuffer);
+            c->wbuffer=NULL;
+        }
+        return -1;
+    }
+    else if(len!=strlen(c->wbuffer))
+    {
+        char *new=malloc(strlen(c->wbuffer)-len+1);
+        char *temp=c->wbuffer+len;
+        new[0]='\0';
+        strcat(new,temp);
+        free(c->wbuffer);
+        c->wbuffer=new;
+        retval=1;
+    } 
+    else
+    {
+        free(c->wbuffer);
+        c->wbuffer=NULL;
+        retval=0;
+    }
+
+    return retval;
+}
+
+void io_unlink(sock c)
+{
+    ios io_data=(ios)c->arg;
+
+    log_debug(ZONE,"Unlinking sock %d from master__list",c->fd);
+
+    if(io_data->master__list==c) io_data->master__list=io_data->master__list->next;
+    if(c->prev!=NULL) c->prev->next=c->next;
+    if(c->next!=NULL) c->next->prev=c->prev;
+}
+
+void io_link(sock c)
+{
+    ios io_data=(ios)c->iodata;
+
+    log_debug(ZONE,"Linking %d, welcome aboard!",c->fd);
+
+    c->next=io_data->master__list;
+    c->prev=NULL;
+    io_data->master__list=c;
+}
+
+void io_close(sock c) 
+{
+    log_debug(ZONE,"client requesting close socket %d",c->fd);
+    c->state=state_CLOSE;
+}
+
+void _io_close(sock c)
+{
+    io_unlink(c);
+    log_debug(ZONE,"closing socket '%d'",c->fd);
+
+    (*(io_cb)c->cb)(c,NULL,0,IO_CLOSED,c->cb_arg);
+
+    if(c->wbuffer!=NULL)free(c->wbuffer);
+    c->wbuffer=strdup("</stream:stream>");
+    _io_write_dump(c);
+    if(c->wbuffer!=NULL)free(c->wbuffer); 
+
+    close(c->fd);
+    pool_free(c->p);
+}
+
+/* write a str to the client socket */
+void io_write_str(sock c,char *buffer)
+{
+    char *new;
+    if(c->wbuffer!=NULL)
+    {
+        new=malloc(strlen(buffer)+strlen(c->wbuffer)+1);
+        new[0]='\0';
+        strcat(new,c->wbuffer);
+        strcat(new,buffer);
+        free(c->wbuffer);
+        c->wbuffer=new;
+    }
+    else
+    {
+        c->wbuffer=strdup(buffer);
+    }
+    _io_write_dump(c);
+}
+
+/* write an xmlnode */
+void io_write(sock c,xmlnode x)
+{
+    ios io_data=(ios)c->iodata;
+    drop d=pmalloco(xmlnode_pool(x),sizeof(_drop));
+    d->x=x;
+    d->c=c;
+    if(io_data->wmp==NULL) io_data->wmp=pth_msgport_create("io_master__wmp");
+    pth_msgport_put(io_data->wmp,(void*)d);
+}
+
+int _io_write(sock c, xmlnode x)
+{
+    char *block;
+
+    log_debug(ZONE,"writing packet [%s] to client socket %d",xmlnode2str(x),c->fd);
+    block = xmlnode2str(x);
+    if(block==NULL) return -1;
+
+    if(c->wbuffer!=NULL)
+    { /* add this to the write buffer */
+        char *new_buffer=malloc(strlen(c->wbuffer)+strlen(block)+1);
+        new_buffer[0]='\0';
+        strcat(new_buffer,c->wbuffer);
+        strcat(new_buffer,block);
+        free(c->wbuffer);
+        c->wbuffer=new_buffer;
+    }
+    else
+    {
+        c->wbuffer=strdup(block);
+    }
+
+    xmlnode_free(x);
+
+    /* write the packet */
+    return _io_write_dump(c);
+}
+
+sock _io_sock(int fd)
+{
+    pool p;
+    sock c;
+
+    p = pool_heap(2*1024);
+    c = pmalloco(p, sizeof(_sock));
+    c->p = p;
+    c->fd = fd;
+    c->state = state_UNKNOWN;
+
+    log_debug(ZONE,"new socket created for fd: %d",fd);
+
+    return c;
+}
+
+sock _io_accept(ios io_data,int asock)
+{
+    struct sockaddr_in sa;
+    size_t sa_size = sizeof(sa);
+    int fd,flags;
+    sock c;
+    fd = accept(asock,(struct sockaddr*)&sa,(int*)&sa_size);
+    if(fd < 0)
+    {
+        return NULL; 
+    }
+
+    (flags=fcntl(fd,F_GETFL,0));
+    flags|=O_NONBLOCK;
+    fcntl(fd,F_SETFL,flags);
+
+    log_debug(ZONE,"pthsock_client: new socket accepted (fd: %d, ip: %s, port: %d)",fd,inet_ntoa(sa.sin_addr),ntohs(sa.sin_port));
+
+    c = _io_sock(fd);
+    c->iodata=(void*)io_data;
+    c->cb=io_data->cb;
+    c->cb_arg=io_data->cb_arg;
+    if(c!=NULL) io_link(c);
+    return c;
+}
+
+void _io_main(void *arg)
+{
+    ios io_data=(ios)arg;
+    pth_event_t wevt;       /* the pth event for the mp */
+    fd_set wfds,rfds, all_wfds,all_rfds; /* writes, reads, all */
+    sock cur, c,temp;    
+    drop d;
+    char buff[1024];
+    int len, asock;
+    int maxfd=0;
+
+    asock = io_data->master_fd;
+    maxfd=asock;
+
+    FD_ZERO(&all_wfds);
+    FD_ZERO(&all_rfds);
+    FD_SET(asock,&all_rfds);
+
+    wevt = pth_event(PTH_EVENT_MSG,io_data->wmp);
+
+    (*(io_cb)io_data->cb)(NULL,NULL,0,IO_INIT,NULL); 
+
+    while (1)
+    {
+        log_debug(ZONE,"%d:%d Watching %d total sockets",asock,maxfd,maxfd-asock);
+        rfds=all_rfds;
+        wfds=all_wfds;
+        pth_select_ev(maxfd+1,&rfds,&wfds,NULL,NULL,wevt);
+
+        pth_mutex_acquire(&m_sync,0,NULL); 
+
+        /* handle packets that need to be written */
+        if (pth_event_occurred(wevt))
+        {
+            while ((d=(drop)pth_msgport_get(io_data->wmp))!=NULL)
+            {
+                int ret;
+                c = d->c;
+
+                if(c->state==state_CLOSE){
+                        xmlnode_free(d->x);
+                        continue;
+                }
+
+                log_debug(ZONE,"write event for %d",c->fd);
+                ret=_io_write(c,d->x);
+                if(ret<0) /* error occured here */
+                {
+                    if(errno!=EWOULDBLOCK)
+                    {
+                        FD_CLR(c->fd,&all_rfds);
+                        FD_CLR(c->fd,&all_wfds);
+                        _io_close(c);
+                        continue;
+                    }
+                    else 
+                    {
+                        FD_SET(c->fd,&all_wfds);
+                    }
+                }
+                else if(ret) /* didn't write all the data */
+                    FD_SET(c->fd,&all_wfds);
+                else /* good write */
+                    FD_CLR(c->fd,&all_wfds);
+            }
+        }
+
+        maxfd=asock;
+
+        FD_ZERO(&all_rfds); /* reset our "all" set */
+        FD_SET(asock,&all_rfds);
+
+        if (FD_ISSET(asock,&rfds)) /* new connection */
+        {
+            c=_io_accept(io_data,asock);
+            if(c!=NULL) 
+            {
+               (*(io_cb)c->cb)(c,NULL,0,IO_NEW,c->cb_arg);
+               FD_SET(c->fd,&all_rfds);           
+               if(c->fd>maxfd)maxfd=c->fd;
+            }
+        }
+
+        cur = io_data->master__list;
+        log_debug(ZONE,"looping through sockets");
+        while(cur != NULL)
+        {
+            FD_SET(cur->fd,&all_rfds);
+            if(cur->state==state_CLOSE)
+            {
+                temp=cur;
+                cur=cur->next;
+                _io_close(temp);
+                FD_CLR(temp->fd,&all_rfds);
+                FD_CLR(temp->fd,&all_wfds);
+                continue;
+            }
+            if (FD_ISSET(cur->fd,&rfds))
+            { /* we need to read from a socket */
+                log_debug(ZONE,"read event for %d",cur->fd);
+                len = read(cur->fd,buff,sizeof(buff));
+                if(len <= 0)
+                {
+                    if(errno!=EWOULDBLOCK)
+                    {
+                        temp=cur;
+                        cur=cur->next;
+                        log_debug(ZONE,"Error reading on '%d', %s",temp->fd,strerror(errno));
+                        FD_CLR(temp->fd,&all_rfds);
+                        FD_CLR(temp->fd,&all_wfds);
+                        _io_close(temp);
+                        continue;
+                    }
+                }
+                else
+                {
+                    log_debug(ZONE,"read %d bytes: %s",len,buff);
+                    (*(io_cb)c->cb)(c,buff,len,IO_NORMAL,c->cb_arg);
+                    xstream_eat(cur->xs,buff,len);
+                }
+            }
+            else if(FD_ISSET(cur->fd,&wfds))
+            { /* ooo, we are ready to dump the rest of the data */
+                int ret=_io_write_dump(cur);
+                log_debug(ZONE,"write event for %d",cur->fd);
+                if(ret<0)
+                {
+                    if(errno!=EWOULDBLOCK)
+                    {
+                        temp=cur;
+                        cur=cur->next;
+                        log_debug(ZONE,"error writing to socket %d:%s",temp->fd,strerror(errno));
+                        FD_CLR(temp->fd,&all_rfds);
+                        FD_CLR(temp->fd,&all_wfds);
+                        _io_close(temp);
+                        continue;
+                    } else FD_SET(cur->fd,&all_wfds);
+                }
+                else if(!ret) FD_CLR(cur->fd,&all_wfds);
+                else FD_SET(cur->fd,&all_wfds);
+            }
+
+            if(cur->wbuffer!=NULL) FD_SET(cur->fd,&all_wfds);
+            else FD_CLR(cur->fd,&all_wfds);
+            if(cur->fd>maxfd)maxfd=cur->fd;
+            cur = cur->next;
+        }
+        pth_mutex_release(&m_sync);
+    }
+    log_debug(ZONE,"This will never get here");
+    pth_event_free(wevt,PTH_FREE_THIS);
+}
+
+/* everything starts here */
+void io_select(int port,io_cb cb,void *arg)
+{
+    ios io_data;
+    pool p=pool_new();
+    pth_attr_t attr;
+    int fd;
+    int flags;
+
+    log_debug(ZONE,"io_select to listen on %d [%X]",port,(void*)cb);
+
+    io_data = pmalloco(p,sizeof(_ios));
+
+    /* write mp */
+    io_data->wmp=pth_msgport_create("io_master__wmp");
+    io_data->p=p;
+    io_data->cb=cb;
+    io_data->cb_arg=arg;
+
+    fd = make_netsocket(port,NULL,NETSOCKET_SERVER);
+    if(fd < 0)
+    {
+        log_error(NULL,"io_select is unable to listen on %d",port);
+        pool_free(p);
+        return;
+    }
+
+    if(listen(fd,10) < 0)
+    {
+        log_error(NULL,"pthsock_client is unable to listen on %d",port);
+        pool_free(p);
+        return;
+    }
+
+    (flags=fcntl(fd,F_GETFL,0));
+    flags|=O_NONBLOCK;
+    fcntl(fd,F_SETFL,flags);
+
+    io_data->master_fd = fd;
+
+    attr = pth_attr_new();
+    pth_attr_set(attr,PTH_ATTR_JOINABLE,FALSE);
+
+    /* start main accept/read/write thread */
+    pth_spawn(attr,(void*)_io_main,(void*)io_data);
+
+    pth_attr_destroy(attr);
+}
