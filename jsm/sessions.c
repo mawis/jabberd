b2664315712efcdf8819eec3341b9b555a7865cc
jeremie <jeremie@localhost>
2000-10-07 19:14:46 +0000

    ok, dnsrv: make sure things start in order deliver: bad log message sessions: enforce the from WITH the resource, naughty clients might send a from="user@server" w/o resource, and errors to those messages get dropped by the offline thread (errors should probably not be dropped either, /me makes note)

--- jsm/sessions.c
+++ jsm/sessions.c
@@ -215,12 +215,8 @@ void _js_session_from(void *arg)
     s->c_out++;
 
     /* make sure we have our from set correctly for outgoing packets */
-    if(jid_cmp(p->from,s->uid) != 0 && jid_cmp(p->from,s->id) != 0)
-    {
-        /* nope, fix it */
-        xmlnode_put_attrib(p->x,"from",jid_full(s->id));
-        p->from = jid_new(p->p,jid_full(s->id));
-    }
+    xmlnode_put_attrib(p->x,"from",jid_full(s->id));
+    p->from = jid_new(p->p,jid_full(s->id));
 
     /* let the modules have their heyday */
     if(js_mapi_call(NULL, es_OUT,  p, s->u, s))
