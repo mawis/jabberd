98f42dd0f80f28f18395832a59fdb7fca1a29f07
tsbandit <tsbandit@localhost>
2000-10-12 23:03:16 +0000

    added comments to help jer with doing dialback

--- jabberd14/pthsock/server.c
+++ jabberd14/pthsock/server.c
@@ -44,16 +44,16 @@ struct sdata_st;
 typedef struct ssi_st
 {
     instance i;
-    HASHTABLE out_tab;
+    HASHTABLE out_tab; /* hash table of all outgoing sockets to hostnames */
 } *ssi, _ssi;
 
 typedef struct sdata_st
 {
-    ssi i;
+    ssi i; /* server.c specific data */
     pool p;
-    conn_type type;
-    pth_msgport_t queue;
-    char *to;
+    conn_type type; /* outgoing or incoming? */
+    pth_msgport_t queue; /* pre-connected write queue */
+    char *to; /* server name connected to */
     void *arg;
 } *sdata, _sdata;
 
@@ -69,16 +69,20 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
     switch(type)
     {
     case XSTREAM_ROOT:
-        if(sd->type==conn_OUT)
+        if(sd->type==conn_OUT) /* if this is an outgoing conn */
         {
             wbq q;
+            /* that means we got the other stream header,
+             * dump the queue, and continue like normal */
             while((q=(wbq)pth_msgport_get(sd->queue))!=NULL)
                 io_write(c,q->x);
+            pth_msgport_destroy(sd->queue); /* no longer used after this */
+            sd->queue=NULL;
             xmlnode_free(x);
             break;
         }
 
-        if(xmlnode_get_attrib(x,"xmlns:etherx")==NULL&&
+        if(xmlnode_get_attrib(x,"xmlns:etherx")==NULL&& /* verify header */
            xmlnode_get_attrib(x,"etherx:secret")==NULL)
         {
             to=xmlnode_get_attrib(x,"to");
@@ -91,7 +95,7 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
                 sd->type = conn_CLOSED;
             }
             else
-            {
+            { /* header is okay, send our header */
                 h=xstream_header("jabber:server",NULL,to);
                 block = xstream_header_char(h);
                 io_write_str(c,block);
@@ -99,7 +103,7 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
             }
         }
         else
-        {
+        { /* don't allow stupid 1.0 transports */
             io_write_str(c,"<stream:error>Transport Access is Denied</stream:error>");
             io_close((sock)sd->arg);
             sd->type = conn_CLOSED;   /* it wants to be a transport, to bad */
@@ -108,13 +112,14 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
         break;
     case XSTREAM_NODE:
         if(sd->type==conn_OUT)
-        {
+        { /* don't allow incoming data on an outgoing conn */
             log_debug(ZONE,"Outgoing connection tried to receive data!");
             io_write_str(c,"<stream:error>This connection does not accept incoming data</stream:error>");
             xmlnode_free(x);
             break;
         }
 
+        /* kill the annoying bits */
         xmlnode_hide_attrib(x,"etherx:from");
         xmlnode_hide_attrib(x,"etherx:to");
         xmlnode_hide_attrib(x,"sto");
@@ -131,12 +136,12 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
             deliver(dpacket_new(x),sd->i->i);
         }
         else
-        {
+        { /* wierd DNS issue.. we connected to ourselves! */
             deliver_fail(dpacket_new(x),"External Server Error");
         }
         break;
     case XSTREAM_ERR:
-        log_debug(ZONE,"Bad XML: %s",(char*)x);
+        log_debug(ZONE,"Bad XML: %s",xmlnode2str(x));
         io_write_str(c,"<stream:error>You sent malformed XML</stream:error>");
     case XSTREAM_CLOSE:
         /* they closed there connections to us */
@@ -146,7 +151,7 @@ void pthsock_server_in(int type, xmlnode x, void *arg)
     }
 }
 
-
+/* callback for io_select */
 void pthsock_server_read(sock c,char *buffer,int bufsz,int flags,void *arg)
 {
     ssi si=(ssi)arg;
@@ -157,8 +162,8 @@ void pthsock_server_read(sock c,char *buffer,int bufsz,int flags,void *arg)
     switch(flags)
     {
     case IO_INIT:
-        break;
-    case IO_NEW:
+        break; /* umm.. who cares? */
+    case IO_NEW: /* new socket from io_select */
         log_debug(ZONE,"NEW server socket connected at %d",c->fd);
         sd=(sdata)c->arg;
         if(sd==NULL)
@@ -182,15 +187,19 @@ void pthsock_server_read(sock c,char *buffer,int bufsz,int flags,void *arg)
             xmlnode_free(x);
             sd->type=conn_OUT;
         }
+        /* create the xstream for the socket */
         c->xs = xstream_new(c->p,(void*)pthsock_server_in,(void*)c);
         break;
     case IO_NORMAL:
+        /* yum yum */
         ret=xstream_eat(c->xs,buffer,bufsz);
         break;
     case IO_CLOSED:
+        /* called JUST BEFORE the socket pool is freed 
+         * after this, the sdata is invalid so clean up! */
         sd=(sdata)c->arg;
         if (sd->type == conn_OUT)
-        {
+        { /* we no longer have an outgoing conn to this server */
             ghash_remove(si->out_tab,sd->to);
             unregister_instance(si->i,sd->to);
         }
@@ -204,13 +213,14 @@ void pthsock_server_read(sock c,char *buffer,int bufsz,int flags,void *arg)
         sd=(sdata)c->arg;
         log_debug(ZONE,"Socket Error to host %s, bouncing queue",sd->to);
         if(c->xbuffer!=NULL)
-        {
+        { /* if there is an xbuffer, there is one or more waiting packet to write */
             if(((int)c->xbuffer)!=-1)
                 deliver_fail(dpacket_new(c->xbuffer),"External Server Error");
             else pool_free(c->pbuffer);
             c->xbuffer=NULL;
             c->wbuffer=c->cbuffer=NULL;
             c->pbuffer=NULL;
+            /* xbuffer is just the first one, the rest are on the mp */
             while((q=(wbq)pth_msgport_get(c->queue))!=NULL)
             {
                 if(q->type==queue_XMLNODE)
@@ -219,12 +229,14 @@ void pthsock_server_read(sock c,char *buffer,int bufsz,int flags,void *arg)
                 } else pool_free(q->p);
             }
         }
-        /* as well as our queue */
-        while((q=(wbq)pth_msgport_get(sd->queue))!=NULL)
-            deliver_fail(dpacket_new(q->x),"External Server Error");
+        /* as well as our pre connected queue */
+        if(sd->queue!=NULL)
+            while((q=(wbq)pth_msgport_get(sd->queue))!=NULL)
+                deliver_fail(dpacket_new(q->x),"External Server Error");
     }
 }
 
+/* phandler callback */
 result pthsock_server_packets(instance id, dpacket dp, void *arg)
 {
     ssi si = (ssi) arg;
@@ -237,27 +249,32 @@ result pthsock_server_packets(instance id, dpacket dp, void *arg)
 
     ip=xmlnode_get_attrib(dp->x,"ip"); /* look for ip="12.34.56.78:5269" header */ 
 
+    /* we don't care about the route header, just the meat */
     if(dp->type==p_ROUTE&&xmlnode_get_firstchild(dp->x)!=NULL)
         dp->x=xmlnode_get_firstchild(dp->x);
-    else if(dp->type==p_ROUTE)
+    else if(dp->type==p_ROUTE) /* ie has no child */
     { /* bad route packet */
         log_notice(si->i->id,"Dropping Invalid Incoming packet: %s",xmlnode2str(dp->x));
         xmlnode_free(dp->x);
         return r_DONE;
     }
 
+    /* maybe the ip is on the internal packet? */
+    if(ip==NULL) ip=xmlnode_get_attrib(dp->x,"ip"); /* look for ip="12.34.56.78:5269" header */ 
+
+    /* get real to and from */
     to=jid_new(xmlnode_pool(dp->x),xmlnode_get_attrib(dp->x,"to"));
     from=jid_new(xmlnode_pool(dp->x),xmlnode_get_attrib(dp->x,"from"));
 
     if(to==NULL)
-    {
+    { /* uh oh! no to.. this cored the server once */
         log_notice(si->i->id,"Dropping Invalid Incoming packet: %s",xmlnode2str(dp->x->parent));
         xmlnode_free(dp->x);
         return r_DONE;
     }
 
     if(ip!=NULL)
-    {
+    { /*grab the IP and port */
         char *colon=strchr(ip,':');
         if(colon==NULL) 
             port=5269;
@@ -269,36 +286,39 @@ result pthsock_server_packets(instance id, dpacket dp, void *arg)
         }
     }
     else
-    {
+    { /*else, we lookup ourselves*/
         ip=to->server;
         port=5269;
     }
 
-    log_debug(ZONE,"pthsock_server looking up %s",ip);
+    log_debug(ZONE,"pthsock_server connecting to %s",ip);
 
+    /* etherx header crap for 1.0 compatibily */
     if (from)
         xmlnode_put_attrib(dp->x,"etherx:from",from->server);
     xmlnode_put_attrib(dp->x,"etherx:to",to->server);
 
+    /* find our outgoing conn to this server */
     sd = ghash_get(si->out_tab,to->server);
 
     if (sd != NULL) /* make sure we found a valid outgoing socket */
         if ((sd->type!=conn_OUT&&sd->type!=conn_CONNECTING)||sd->arg==NULL||((sock)sd->arg)->state!=state_ACTIVE)
-            sd = NULL;
+            sd = NULL; /* nope it's not really valid */
 
     q=pmalloco(dp->p,sizeof(_wbq));
     q->x=dp->x;
-    xmlnode_hide_attrib(q->x,"etherx:from");
-    xmlnode_hide_attrib(q->x,"etherx:to");
-    xmlnode_hide_attrib(q->x,"sto");
-    xmlnode_hide_attrib(q->x,"sfrom");
+    /* hide the header crap */
+    xmlnode_hide_attrib(q->x,"sto");   /* XXX can we kill these yet? */
+    xmlnode_hide_attrib(q->x,"sfrom"); /* XXX can we kill these yet? */
     xmlnode_hide_attrib(q->x,"ip");
     xmlnode_hide_attrib(q->x,"iperror");
 
+    /* no current socket found for this host */
     if (sd == NULL)
     {
         log_debug(ZONE,"Creating new connection to %s",to->server);
 
+        /* create the sdata to attach to the new socket */
         p = pool_new();
         sd = pmalloco(p,sizeof(_sdata));
         sd->p=p;
@@ -306,16 +326,19 @@ result pthsock_server_packets(instance id, dpacket dp, void *arg)
         sd->to = pstrdup(p,to->server);
         sd->i = si;
         sd->queue = pth_msgport_create("queue");
+        /* add this to our outgoing list, so no more sockets get opened */
         ghash_put(si->out_tab,sd->to,sd);
 
+        /* pop on the queue, and spawn a connection thread */
         pth_msgport_put(sd->queue,(void*)q);
         io_select_connect(ip,port,(void*)sd,pthsock_server_read,(void*)si);
         return r_DONE;
     }
 
+    /* if we haven't connected fully, write to the temp queue */
     if(sd->type==conn_CONNECTING)
         pth_msgport_put(sd->queue,(void*)q);
-    else if(sd->type==conn_OUT)
+    else if(sd->type==conn_OUT) /* otherwise,just write the sucker */
         io_write((sock)sd->arg,dp->x);
 
     return r_DONE;
@@ -334,6 +357,7 @@ void pthsock_server(instance i, xmlnode x)
     si->out_tab = ghash_create(20,(KEYHASHFUNC)str_hash_code,(KEYCOMPAREFUNC)j_strcmp);
     si->i=i;
 
+    /* XXX make configurable rate limits */
     io_select_listen(5269,NULL,pthsock_server_read,(void*)si,5,25);
     register_phandler(i,o_DELIVER,pthsock_server_packets,(void*)si);
 }
