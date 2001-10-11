ef02391b26540354e98c3347913baf89be8ec208
jeremie <jeremie@localhost>
2001-10-11 08:14:08 +0000

    here's the cygwin folder and all it's goodies... this is an awful way to integrate a new platform, but it's easy, and it keeps things seperate :)

---
+++ cygwin/dnsrv/srv_resolv.h
@@ -0,0 +1,35 @@
+/* --------------------------------------------------------------------------
+ *
+ * License
+ *
+ * The contents of this file are subject to the Jabber Open Source License
+ * Version 1.0 (the "License").  You may not copy or use this file, in either
+ * source code or executable form, except in compliance with the License.  You
+ * may obtain a copy of the License at http://www.jabber.com/license/ or at
+ * http://www.opensource.org/.  
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Copyrights
+ * 
+ * Portions created by or assigned to Jabber.com, Inc. are 
+ * Copyright (c) 1999-2000 Jabber.com, Inc.  All Rights Reserved.  Contact
+ * information for Jabber.com, Inc. is available at http://www.jabber.com/.
+ *
+ * Portions Copyright (c) 1998-1999 Jeremie Miller.
+ * 
+ * Acknowledgements
+ * 
+ * Special thanks to the Jabber Open Source Contributors for their
+ * suggestions and support of Jabber.
+ * 
+ * --------------------------------------------------------------------------*/
+#ifndef INCL_SRV_RESOLV_H
+#define INCL_SRV_RESOLV_H
+
+char* srv_lookup(pool p, const char* service, const char* domain);
+
+#endif
