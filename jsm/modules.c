6a432ae0c42576d19ab5ce4465f912d39d60be09
jeremie <jeremie@localhost>
2002-02-04 22:36:08 +0000

    well, I checked and double-checked to make sure this was stamped correctly on all the right files, hopefully that was enough :)

--- jsm/modules.c
+++ jsm/modules.c
@@ -3,20 +3,20 @@
  * License
  *
  * The contents of this file are subject to the Jabber Open Source License
- * Version 1.0 (the "License").  You may not copy or use this file, in either
- * source code or executable form, except in compliance with the License.  You
- * may obtain a copy of the License at http://www.jabber.com/license/ or at
+ * Version 1.0 (the "JOSL").  You may not copy or use this file, in either
+ * source code or executable form, except in compliance with the JOSL. You
+ * may obtain a copy of the JOSL at http://www.jabber.org/ or at
  * http://www.opensource.org/.  
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
+ * Software distributed under the JOSL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the JOSL
  * for the specific language governing rights and limitations under the
- * License.
+ * JOSL.
  *
  * Copyrights
  * 
  * Portions created by or assigned to Jabber.com, Inc. are 
- * Copyright (c) 1999-2000 Jabber.com, Inc.  All Rights Reserved.  Contact
+ * Copyright (c) 1999-2002 Jabber.com, Inc.  All Rights Reserved.  Contact
  * information for Jabber.com, Inc. is available at http://www.jabber.com/.
  *
  * Portions Copyright (c) 1998-1999 Jeremie Miller.
@@ -24,6 +24,17 @@
  * Acknowledgements
  * 
  * Special thanks to the Jabber Open Source Contributors for their
+ * suggestions and support of Jabber.
+ * 
+ * Alternatively, the contents of this file may be used under the terms of the
+ * GNU General Public License Version 2 or later (the "GPL"), in which case
+ * the provisions of the GPL are applicable instead of those above.  If you
+ * wish to allow use of your version of this file only under the terms of the
+ * GPL and not to allow others to use your version of this file under the JOSL,
+ * indicate your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL.  If you do not
+ * delete the provisions above, a recipient may use your version of this file
+ * under either the JOSL or the GPL. 
  * suggestions and support of Jabber.
  * 
  * modules.c - jsm module API
