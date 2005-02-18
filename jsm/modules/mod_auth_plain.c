4bea5ff1080d06891d76b43378faa997b23a985b
Matthias Wimmer <m@tthias.eu>
2005-02-18 22:13:58 +0000

    The 2004-09-24 change where better checking for jabber:iq:register requests was introduced disabled unregistering an account on the other side. This should enable unregistering accounts again. I added a long comment about the xdb-set check action in xdb_file/xdb_file.c as I suspect a bug to be there. (And added references in xdb.c and mod_auth_plain.c)

--- jsm/modules/mod_auth_plain.c
+++ jsm/modules/mod_auth_plain.c
@@ -40,6 +40,30 @@
  * --------------------------------------------------------------------------*/
 #include "jsm.h"
 
+/**
+ * @file mod_auth_plain.c
+ * @brief handles authentication using plaintext password with plaintext passwords in xdb
+ *
+ * This module is responsible for handling authentication if the client uses
+ * legacy plaintext authentication. It stores and uses plaintext password in xdb.
+ * mod_auth_crypt.c is available as a replacement for mod_auth_plain.c, if only hash
+ * values of the password should be stored in xdb. But this means that many more advanced
+ * authentication methods won't work if you only store hashes.
+ *
+ * Using plaintext password in xdb also will make it much more easy to convert your
+ * server to use SASL in the future.
+ */
+
+/**
+ * handle authentication requests
+ *
+ * get requests are used to check which authentication methods are available
+ * set requests are the actual authentication requests
+ *
+ * @param m the mapi structure, containing the authentication request
+ * @param arg unused / ignored
+ * @return M_PASS if the next module should be called to handle the request, M_HANDLED if the request has been completely handled
+ */
 mreturn mod_auth_plain_jane(mapi m, void *arg)
 {
     char *pass;
@@ -67,6 +91,7 @@ mreturn mod_auth_plain_jane(mapi m, void *arg)
 
     log_debug2(ZONE, LOGT_AUTH, "trying xdb act check");
     /* if the act "check" fails, PASS so that 0k could use the password to try and auth w/ it's data */
+    /* XXX see the comment in xdb_file/xdb_file.c for the check action */
     if(xdb_act(m->si->xc, m->user->id, NS_AUTH, "check", NULL, xmlnode_get_tag(m->packet->iq,"password")))
         return M_PASS;
 
@@ -74,6 +99,14 @@ mreturn mod_auth_plain_jane(mapi m, void *arg)
     return M_HANDLED;
 }
 
+/**
+ * save the new password of a user in xdb
+ *
+ * @param m the mapi instance
+ * @param id for which user to set the password
+ * @param pass the new password (wrapped in a password element of the right namespace)
+ * @return 0 if setting the password succeded, it failed otherwise
+ */
 int mod_auth_plain_reset(mapi m, jid id, xmlnode pass)
 {
     log_debug2(ZONE, LOGT_AUTH, "resetting password");
@@ -82,7 +115,19 @@ int mod_auth_plain_reset(mapi m, jid id, xmlnode pass)
     return xdb_set(m->si->xc, id, NS_AUTH, pass);
 }
 
-/* handle saving the password for registration */
+/**
+ * handle saving the password
+ *
+ * used if the user just registers his account or if the user changed
+ * his password
+ *
+ * get requests are used to check if authentication type is supported
+ * set requests are used to set the password
+ *
+ * @param m the mapi instance containing the query
+ * @param arg unused/ignored
+ * @return M_HANDLED if we handled the request (or rejected it), H_PASS else
+ */
 mreturn mod_auth_plain_reg(mapi m, void *arg)
 {
     jid id;
@@ -96,6 +141,11 @@ mreturn mod_auth_plain_reg(mapi m, void *arg)
 
     /* only handle set requests (get requests already have been handled) */
     if(jpacket_subtype(m->packet) != JPACKET__SET) return M_PASS;
+
+    /* do not handle/reject unregister requests */
+    if (xmlnode_get_tag(m->packet->iq, "remove") != NULL) {
+	return M_PASS;
+    }
 
     /* take care, that there is a new password) */
     if((pass = xmlnode_get_tag(m->packet->iq,"password")) == NULL
@@ -131,9 +181,18 @@ mreturn mod_auth_plain_reg(mapi m, void *arg)
     return M_PASS;
 }
 
-/* handle password change requests from a session */
-mreturn mod_auth_plain_server(mapi m, void *arg)
-{
+/**
+ * handle password change requests from a session
+ *
+ * This function handles stanzas sent to the server address, this are
+ * password change requests after the user already authenticated - it
+ * does not handle normal user registration.
+ *
+ * @param m the mapi instance, containing the user's request
+ * @param arg unused / ignored
+ * @return M_IGNORE if it is no iq stanza, M_HANDLED if we handled the password change, M_PASS else
+ */
+mreturn mod_auth_plain_server(mapi m, void *arg) {
     mreturn ret;
 
     /* pre-requisites */
@@ -149,9 +208,21 @@ mreturn mod_auth_plain_server(mapi m, void *arg)
     return ret;
 }
 
+
+/**
+ * register this module in the session manager
+ *
+ * will register the following callbacks in the session manager:
+ * - mod_auth_plain_jane as authentication handler
+ * - mod_auth_plain_server to process packets sent to the server address
+ * - mod_auth_plain_reg to process data of registering users (only if there is
+ *   a <register/> element in the session manager configuration)
+ *
+ * @param si the session manager instance
+ */
 void mod_auth_plain(jsmi si)
 {
-    log_debug2(ZONE, LOGT_INIT, "init");
+    log_debug2(ZONE, LOGT_INIT, "mod_auth_plain is initializing");
 
     js_mapi_register(si, e_AUTH, mod_auth_plain_jane, NULL);
     js_mapi_register(si, e_SERVER, mod_auth_plain_server, NULL);
