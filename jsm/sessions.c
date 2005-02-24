0e405d398ef6dc8daadfc4b1b574ddd56ce10c82
Matthias Wimmer <m@tthias.eu>
2005-02-24 00:27:14 +0000

    Code documentation updates

--- jsm/sessions.c
+++ jsm/sessions.c
@@ -36,19 +36,33 @@
  * delete the provisions above, a recipient may use your version of this file
  * under either the JOSL or the GPL. 
  * 
- * 
- * sessions.c -- handle messages to and from user sessions
- *
  * --------------------------------------------------------------------------*/
+
+/**
+ * @file sessions.c
+ * @brief handle messages to and from user sessions
+ */
 
 #include "jsm.h"
 
+/* forward declarations */
 void _js_session_start(void *arg);
 void _js_session_to(void *arg);
 void _js_session_from(void *arg);
 void _js_session_end(void *arg);
 
-/* delivers a route packet to all listeners for this session */
+/**
+ * deliver a packet to the client connection manager responsible for the session
+ *
+ * The packet is wrapped inside a <route/> element and routed to
+ * the client connection manager, that handles the connection for the
+ * sepcified session. If the xmlnode in is NULL, a <route type='error'/>
+ * stanza is sent to the client connection manager, signalling that the
+ * session is not valid anymore.
+ *
+ * @param s the session to which the packet should be routed to
+ * @param in the stanza that should be routed to the c2s for the session
+ */
 void js_session_route(session s, xmlnode in)
 {
     /* NULL means this is an error from the session ending */
@@ -66,17 +80,17 @@ void js_session_route(session s, xmlnode in)
     deliver(dpacket_new(in), s->si->i);
 }
 
-/*
- *  js_session_new -- creates a new session, registers the resource for it
- *  
- *  Sets up all the data associated with the new session, then send it a 
- *  start spacket, which basically notifies all modules about the new session
+/**
+ * create a new session, register the resource for it
  *
- *  returns
- *      a pointer to the new session 
+ * Sets up all the data associated with a new session, then
+ * notify all modules that registered for e_SESSION about the newly created session
+ *
+ * @param si the session manager instance data
+ * @param dp the packet we received from the c2s instance, that requested the new session
+ * @return a pointer to the new session (NULL if input data is invalid)
  */
-session js_session_new(jsmi si, dpacket dp)
-{
+session js_session_new(jsmi si, dpacket dp) {
     pool p;         /* a memory pool for the session */
     session s, cur;      /* the session being created */
     int i;
@@ -133,20 +147,17 @@ session js_session_new(jsmi si, dpacket dp)
     return s;
 }
 
-/*
- *  js_session_end -- shut down the session
- *  
- *  This function gets called when the user disconnects or when the server shuts
- *  down. It changes the user's presence to offline, cleans up the session data
- *  and sends an end spacket
+/**
+ * shut down the session
  *
- *  parameters
- *  	s -- the session to end
- *      reason -- the reason the session is shutting down (for logging)
+ * This function gets called when the user disconnects or when the server shuts down.
+ * It changes the user's presence to offline, cleans up the session data and notifies
+ * all registered modules for the es_END event about the closed session
  *
+ * @param s the session, that is closing
+ * @param reason textual reason for the shutdown
  */
-void js_session_end(session s, char *reason)
-{
+void js_session_end(session s, char *reason) {
     xmlnode x;      /* new presence data */
     session cur;    /* used to iterate over the user's session list
                        when removing the session from the list */
@@ -165,8 +176,7 @@ void js_session_end(session s, char *reason)
     s->priority = -129;
 
     /* if the last known presence was available, update it */
-    if(s->presence != NULL && j_strcmp(xmlnode_get_attrib(s->presence, "type"), "unavailable") != 0)
-    {
+    if(s->presence != NULL && j_strcmp(xmlnode_get_attrib(s->presence, "type"), "unavailable") != 0) {
 
         /* create a new presence packet with the reason the user is unavailable */
         x = jutil_presnew(JPACKET__UNAVAILABLE,NULL,reason);
@@ -184,17 +194,13 @@ void js_session_end(session s, char *reason)
      * remove this session from the user's session list --
      * first check if this session is at the head of the list
      */
-    if(s == s->u->sessions)
-    {
+    if(s == s->u->sessions) {
         /* yup, just bump up the next session */
         s->u->sessions = s->next;
-
-    }else{
-
+    } else {
         /* no, we have to traverse the list to find it */
         for(cur = s->u->sessions; cur->next != s; cur = cur->next);
         cur->next = s->next;
-
     }
 
     /* so it doesn't get freed */
@@ -202,10 +208,15 @@ void js_session_end(session s, char *reason)
 
     /* tell it to exit */
     mtq_send(s->q, s->p, _js_session_end, (void *)s);
-
 }
 
-/* child that starts a session */
+/**
+ * child that starts a session
+ *
+ * Calles all registered modules for the event e_SESSION and notifies them about the newly created session
+ *
+ * @param arg the newly created session
+ */
 void _js_session_start(void *arg)
 {
     session s = (session)arg;
@@ -217,7 +228,15 @@ void _js_session_start(void *arg)
     s->started = time(NULL);
 }
 
-/* child that handles packets from the user */
+/**
+ * Child that handles packets we just received from the client connection manager from one of our users
+ *
+ * It is checked first if the packet is of a known type, if not it gets rejected.
+ * For known packets it is first tried if one of the registered modules for es_OUT handles the packet,
+ * iff none of the modules handles the packet, it is further passed to js_deliver().
+ *
+ * @param arg the packet we just received
+ */
 void _js_session_from(void *arg)
 {
     jpacket p = (jpacket)arg;
@@ -225,16 +244,14 @@ void _js_session_from(void *arg)
     jid uid;
 
     /* if this session is dead */
-    if(s->exit_flag)
-    {
+    if(s->exit_flag) {
         /* send the packet into oblivion */
         xmlnode_free(p->x);
         return;
     }
 
     /* at least we must have a valid packet */
-    if(p->type == JPACKET_UNKNOWN)
-    {
+    if(p->type == JPACKET_UNKNOWN) {
         /* send an error back */
         jutil_error_xmpp(p->x,XTERROR_BAD);
         jpacket_reset(p);
@@ -280,15 +297,26 @@ void _js_session_from(void *arg)
 
 }
 
-/* child that handles packets to the user */
+/**
+ * child that handles packets to a specified resource (session) of a user
+ *
+ * It is checked if the session the packet is addressed to has the exit_flag set,
+ * if so it is sent back (?) to js_deliver(). If the exit_flag is not set
+ * it is first tried if one of the registered callbacks for es_IN handles the
+ * packet. Iff not the exit_flag is checked again (and probably the packet sent
+ * back to js_deliver()) and if it is still not set, the packet is passed to
+ * js_session_route() which delivers it to the c2s for sending it to the
+ * connection related to the session.
+ *
+ * @param arg the packet
+ */
 void _js_session_to(void *arg)
 {
     jpacket p = (jpacket)arg;
     session s = (session)(p->aux1);
 
     /* if this session is dead... */
-    if(s->exit_flag)
-    {
+    if(s->exit_flag) {
         /* ... and the packet is a message */
         if(p->type == JPACKET_MESSAGE)
             js_deliver(s->si, p);
@@ -322,7 +350,14 @@ void _js_session_to(void *arg)
     js_session_route(s, p->x);
 }
 
-/* child that cleans up a session */
+/**
+ * child that cleans up a session
+ *
+ * Notify the responsible c2s instance about the closed session.
+ * Call the modules that registered for the es_END event, to notify them about the closed session
+ *
+ * @param arg the session, that is closing
+ */
 void _js_session_end(void *arg)
 {
     session s = (session)arg;
@@ -350,22 +385,17 @@ void _js_session_end(void *arg)
     pool_free(s->p);
 }
 
-/*
- *  js_session_get -- find the session for a resource
- *  
- *  Given a user and a resource, find the corresponding session
- *  if the user is logged in. Otherwise return NULL.
+/**
+ * find the session for a given resource
  *
- *  parameters
- *  	user -- the user's udata record
- *      res -- the resource to search for
+ * Given a user and a resource, find the corresponding session
+ * if the user is logged in. Otherweise return NULL.
  *
- *  returns
- *      a pointer to the session if the user is logged in
- *      NULL if the user isn't logged in on this resource
+ * @param user the user's udata record
+ * @param res the resource to search for
+ * @return a pointer to the session if the user is logged in, NULL if the user isn't logged in on this resource
  */
-session js_session_get(udata user, char *res)
-{
+session js_session_get(udata user, char *res) {
     session cur;    /* session pointer */
 
     /* screen out illeagal calls */
@@ -384,27 +414,21 @@ session js_session_get(udata user, char *res)
 
     /* if we got this far, there is no session */
     return NULL;
-
 }
 
-/*
- *  js_session_primary -- find the primary session for the user
- *  
- *  Scan through a user's sessions to find the session with the
- *  highest priority and return a pointer to it.
+/**
+ * find the primary session for the user
  *
- *  parameters
- *  	user -- user data for the user in question
+ * Scan through the user's sessions to find the session with the
+ * highest priority and return a pointer to it.
  *
- *  returns
- *      a pointer to the primary session if the user is logged in
- *      NULL if there are no active sessions
+ * @param user the user to find the highest session for
+ * @return pointer to the primary session if the user is logged in with at least priority 0, NULL if there is no active session with a priority of at least 0
  */
-session js_session_primary(udata user)
-{
+session js_session_primary(udata user) {
     session cur, top;
 
-    /* ignore illeagal calls, or users with no sessions */
+    /* ignore illegal calls, or users with no sessions */
     if(user == NULL || user->sessions == NULL)
         return NULL;
 
@@ -415,14 +439,21 @@ session js_session_primary(udata user)
             top = cur;
 
     /* return it if it's active */
-    if(top->priority >= 0)
+    if(top->priority >= 0) {
         return top;
 
-    else /* otherwise there's no active session */
-        return NULL;
-
+    /* otherwise there's no active session */
+    return NULL;
 }
 
+/**
+ * handle packets addresses to a specific resource (session) of a user
+ *
+ * pass them to _js_session_to() ...
+ *
+ * @param s the session the packet is addressed to
+ * @param p the packet
+ */
 void js_session_to(session s, jpacket p)
 {
     /* XXX: Florian had on his server the case where mod_offline.c called this function
@@ -440,6 +471,16 @@ void js_session_to(session s, jpacket p)
     mtq_send(s->q, p->p, _js_session_to, (void *)p);
 }
 
+/**
+ * handle normal stanzas received inside a <route/> stanza from
+ * jabberd. These are packets we get from the client connection manager,
+ * that are from one of our users.
+ *
+ * Pass them to _js_session_from() ...
+ *
+ * @param s the session of the user
+ * @param p the received packet
+ */
 void js_session_from(session s, jpacket p)
 {
     /* check the provided parameters */
@@ -452,4 +493,3 @@ void js_session_from(session s, jpacket p)
     p->aux1 = (void *)s;
     mtq_send(s->q, p->p, _js_session_from, (void *)p);
 }
-
