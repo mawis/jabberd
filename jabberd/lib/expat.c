e0728830d6a4c449d6a82c073d5d62341a415c7d
Matthias Wimmer <m@tthias.eu>
2005-11-28 11:32:40 +0000

    Parsing strings and files with a namespace aware instance of expat. This affects the jabber.xml config file, we now have to declare our prefixes!

--- jabberd/lib/expat.c
+++ jabberd/lib/expat.c
@@ -57,6 +57,16 @@
 #include <jabberdlib.h>
 
 /**
+ * structure used to pass information to the expat callbacks
+ */
+typedef struct {
+    xmlnode 		x;		/**< pointer to the xmlnode that is currently read */
+    ns_list_item 	first_ns;	/**< first element in the list of declared namespace prefixes */
+    ns_list_item 	last_ns;	/**< last element in the list of declared namespace prefixes */
+    pool		parse_pool;	/**< memory pool used while parsing */
+} _expat_callback_data, *expat_callback_data;
+
+/**
  * callback function used for start elements
  *
  * This function is used internally by expat.c as a callback function
@@ -67,24 +77,60 @@
  * @param name name of the starting element
  * @param atts attributes that are contained in the start element
  */
-void expat_startElement(void* userdata, const char* name, const char** atts)
-{
-    /* get the xmlnode pointed to by the userdata */
-    xmlnode *x = userdata;
-    xmlnode current = *x;
+void expat_startElement(void* userdata, const char* name, const char** atts) {
+    char *prefix = NULL;
+    char *ns_iri = NULL;
+    char *local_name = NULL;
 
-    if (current == NULL)
-    {
+    /* get the data we are working on */
+    expat_callback_data callback_data = (expat_callback_data)userdata;
+
+    /* get prefix, iri, and local name of the element */
+    if (strchr(name, XMLNS_SEPARATOR) != NULL) {
+	/* expat found the namespace IRI for us */
+	ns_iri = pstrdup(callback_data->parse_pool, name);
+	local_name = strchr(ns_iri, XMLNS_SEPARATOR);
+	local_name[0] = 0;
+	local_name++;
+	prefix = pstrdup(callback_data->parse_pool, xmlnode_list_get_nsprefix(callback_data->last_ns, ns_iri));
+    } else if (strchr(name, ':') != NULL) {
+	/* expat could not expand the prefix, it's not declared */
+
+	/* ... be liberal in what you accept ... */
+
+	/* start with a guess */
+	prefix = pstrdup(callback_data->parse_pool, name);
+	local_name = strchr(prefix, ':');
+	local_name[0] = 0;
+	local_name++;
+	ns_iri = "http://jabberd.org/no/clue";
+
+	/* some well known prefixes (but they would have to been declared!) */
+	if (j_strcmp(prefix, "stream") == 0) {
+	    ns_iri = NS_STREAM;
+	} else if (j_strcmp(prefix, "db") == 0) {
+	    ns_iri = NS_DIALBACK;
+	}
+    } else {
+	/* default namespace, but not declared */
+
+	/* ... be liberal in what you accept ... (guessing it's 'jabber:server') */
+	prefix = NULL;
+	ns_iri = "jabber:server";
+	local_name = pstrdup(callback_data->parse_pool, name);
+    }
+
+    if (prefix != NULL && prefix[0] == '\0')
+	prefix = NULL;
+
+    if (callback_data->x == NULL) {
         /* allocate a base node */
-        current = xmlnode_new_tag(name);
-        xmlnode_put_expat_attribs(current, atts);
-        *x = current;
+	callback_data->x = xmlnode_new_tag_ns(local_name, prefix, ns_iri);
+    } else {
+	/* insert as child node */
+	callback_data->x = xmlnode_insert_tag_ns(callback_data->x, local_name, prefix, ns_iri);
     }
-    else
-    {
-        *x = xmlnode_insert_tag(current, name);
-        xmlnode_put_expat_attribs(*x, atts);
-    }
+    xmlnode_put_expat_attribs(callback_data->x, atts);
 }
 
 /**
@@ -97,19 +143,20 @@ void expat_startElement(void* userdata, const char* name, const char** atts)
  * @param userdata pointer to the current xmlnode
  * @param name name of the ending element (ignored by this function)
  */
-void expat_endElement(void* userdata, const char* name)
-{
-    xmlnode *x = userdata;
-    xmlnode current = *x;
+void expat_endElement(void* userdata, const char* name) {
+    xmlnode parent = NULL;
+    
+    /* get the data we are working on */
+    expat_callback_data callback_data = (expat_callback_data)userdata;
 
     /*
-    current->complete = 1;
+    callback_data->x->complete = 1;
     */
-    current = xmlnode_get_parent(current);
+    parent = xmlnode_get_parent(callback_data->x);
 
     /* if it's NULL we've hit the top folks, otherwise back up a level */
-    if(current != NULL)
-        *x = current;
+    if(parent != NULL)
+	callback_data->x = parent;
 }
 
 /**
@@ -121,12 +168,44 @@ void expat_endElement(void* userdata, const char* name)
  * @param s pointer to the CDATA string (not zero terminated!)
  * @param len length of the CDATA string
  */
-void expat_charData(void* userdata, const char* s, int len)
-{
-    xmlnode *x = userdata;
-    xmlnode current = *x;
+void expat_charData(void* userdata, const char* s, int len) {
+    /* get the data we are working on */
+    expat_callback_data callback_data = (expat_callback_data)userdata;
 
-    xmlnode_insert_cdata(current, s, len);
+    xmlnode_insert_cdata(callback_data->x, s, len);
+}
+
+/**
+ * callback function for the beginning of a namespace declaration
+ *
+ * This function will insert a new namespace prefix in the list of declared namespaces
+ *
+ * @param userdata XXX
+ * @param prefix prefix that gets declared
+ * @param iri namespace IRI for this prefix
+ */
+void expat_startNamespaceDecl(void *userdata, const XML_Char *prefix, const XML_Char *iri) {
+    /* get the data we are working on */
+    expat_callback_data callback_data = (expat_callback_data)userdata;
+
+    /* store the new prefix in the list */
+    xmlnode_update_decl_list(callback_data->parse_pool, &(callback_data->first_ns), &(callback_data->last_ns), pstrdup(callback_data->parse_pool, prefix), pstrdup(callback_data->parse_pool, iri));
+}
+
+/**
+ * callback function for the end of the scope of a declared namespace prefix
+ *
+ * This function will insert the last occurance of the prefix from the list of declared namespaces
+ *
+ * @param userdata XXX
+ * @param prefix prefix that gets undeclared
+ */
+void expat_endNamespaceDecl(void *userdata, const XML_Char *prefix) {
+    /* get the data we are working on */
+    expat_callback_data callback_data = (expat_callback_data)userdata;
+
+    /* remove the prefix from the list */
+    xmlnode_delete_last_decl(&(callback_data->first_ns), &(callback_data->last_ns), prefix);
 }
 
 /**
@@ -138,31 +217,27 @@ void expat_charData(void* userdata, const char* s, int len)
  * @param len the length of the string (without the zero byte, if present)
  * @return the graph of xmlnodes that represent the parsed document, NULL on failure
  */
-xmlnode xmlnode_str(char *str, int len)
-{
+xmlnode xmlnode_str(char *str, int len) {
     XML_Parser p;
-    xmlnode *x, node; /* pointer to an xmlnode */
+    _expat_callback_data callback_data = { NULL, NULL, NULL, NULL };
 
     if(NULL == str)
         return NULL;
 
-    x = malloc(sizeof(void *));
-
-    *x = NULL; /* pointer to NULL */
-    p = XML_ParserCreate(NULL);
-    XML_SetUserData(p, x);
+    callback_data.parse_pool = pool_new();
+    p = XML_ParserCreateNS(NULL, XMLNS_SEPARATOR);
+    XML_SetUserData(p, &callback_data);
     XML_SetElementHandler(p, expat_startElement, expat_endElement);
     XML_SetCharacterDataHandler(p, expat_charData);
-    if(!XML_Parse(p, str, len, 1))
-    {
+    XML_SetNamespaceDeclHandler(p, expat_startNamespaceDecl, expat_endNamespaceDecl);
+    if(!XML_Parse(p, str, len, 1)) {
         /*        jdebug(ZONE,"xmlnode_str_error: %s",(char *)XML_ErrorString(XML_GetErrorCode(p)));*/
-        xmlnode_free(*x);
-        *x = NULL;
+        xmlnode_free(callback_data.x);
+	callback_data.x = NULL;
     }
-    node = *x;
-    free(x);
     XML_ParserFree(p);
-    return node; /* return the xmlnode x points to */
+    pool_free(callback_data.parse_pool);
+    return callback_data.x; /* return the xmlnode x points to */
 }
 
 /**
@@ -173,10 +248,9 @@ xmlnode xmlnode_str(char *str, int len)
  * @param file the filename
  * @return the graph of xmlnodes that represent the parsed document, NULL on failure
  */
-xmlnode xmlnode_file(char *file)
-{
+xmlnode xmlnode_file(char *file) {
     XML_Parser p;
-    xmlnode *x, node; /* pointer to an xmlnode */
+    _expat_callback_data callback_data = { NULL, NULL, NULL, NULL };
     char buf[BUFSIZ];
     int done, fd, len;
 
@@ -187,30 +261,28 @@ xmlnode xmlnode_file(char *file)
     if(fd < 0)
         return NULL;
 
-    x = malloc(sizeof(void *));
-
-    *x = NULL; /* pointer to NULL */
-    p = XML_ParserCreate(NULL);
-    XML_SetUserData(p, x);
+    callback_data.parse_pool = pool_new();
+    p = XML_ParserCreateNS(NULL, XMLNS_SEPARATOR);
+    XML_SetUserData(p, &callback_data);
     XML_SetElementHandler(p, expat_startElement, expat_endElement);
     XML_SetCharacterDataHandler(p, expat_charData);
-    do{
+    XML_SetNamespaceDeclHandler(p, expat_startNamespaceDecl, expat_endNamespaceDecl);
+    do {
         len = read(fd, buf, BUFSIZ);
         done = len < BUFSIZ;
         if(!XML_Parse(p, buf, len, done))
         {
             /*            jdebug(ZONE,"xmlnode_file_parseerror: %s",(char *)XML_ErrorString(XML_GetErrorCode(p)));*/
-            xmlnode_free(*x);
-            *x = NULL;
+            xmlnode_free(callback_data.x);
+	    callback_data.x = NULL;
             done = 1;
         }
-    }while(!done);
+    } while (!done);
 
-    node = *x;
     XML_ParserFree(p);
-    free(x);
     close(fd);
-    return node; /* return the xmlnode x points to */
+    pool_free(callback_data.parse_pool);
+    return callback_data.x; /* return the xmlnode x points to */
 }
 
 /**
@@ -234,7 +306,7 @@ char* xmlnode_file_borked(char *file) {
     if(fd < 0)
         return "unable to open file";
 
-    p = XML_ParserCreate(NULL);
+    p = XML_ParserCreateNS(NULL, XMLNS_SEPARATOR);
     while(!done)
     {
         len = read(fd, buf, BUFSIZ);
