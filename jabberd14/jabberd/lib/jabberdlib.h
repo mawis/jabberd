227ba805ed1fa6cbb102bef3bcd67711ab875103
mawis <mawis@localhost>
2005-12-13 01:16:55 +0000

    Passing full stanza requesting subscription, even if the subscription request is stored offline

--- jabberd14/jabberd/lib/jabberdlib.h
+++ jabberd14/jabberd/lib/jabberdlib.h
@@ -840,6 +840,7 @@ typedef struct xterror_struct
 #define NS_XMPP_SASL "urn:ietf:params:xml:ns:xmpp-sasl"
 
 #define NS_JABBERD_STOREDPRESENCE "http://jabberd.org/ns/storedpresence"
+#define NS_JABBERD_STOREDREQUEST "http://jabberd.org/ns/storedsubsciptionrequest"
 #define NS_JABBERD_HISTORY "http://jabberd.org/ns/history"
 #define NS_JABBERD_HASH "http://jabberd.org/ns/hash"			/**< namespace for storing xhash data */
 #define NS_JABBERD_XDB "http://jabberd.org/ns/xdb"			/**< namespace for the root element used by xdb_file to store data in files */
