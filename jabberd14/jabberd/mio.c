e229a568eff1fec367785aa662e7cb1c2a1edab5
Matthias Wimmer <m@tthias.eu>
2006-09-02 20:56:51 +0000

    Backport of bug #66 fix to 1.4.4 - RC 1 for 1.4.4.1

--- jabberd14/jabberd/mio.c
+++ jabberd14/jabberd/mio.c
@@ -592,6 +592,13 @@ mio _mio_accept(mio m)
         return NULL;
     }
 
+    /* do not accept a higher fd than FD_SET, or FD_CLR can handle */
+    if (fd >= FD_SETSIZE) {
+	log_warn(NULL, "could not accept incomming connection, maximum number of connections reached (%i)", FD_SETSIZE);
+	close(fd);
+	return NULL;
+    }
+
 #ifdef WITH_IPV6
     allow = _mio_allow_check(inet_ntop(AF_INET6, &serv_addr.sin6_addr, addr_str, sizeof(addr_str)));
     deny = _mio_deny_check(addr_str);
