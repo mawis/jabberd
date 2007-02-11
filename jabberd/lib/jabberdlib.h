c918a028dce4deaaf49696edd9fb62c672fa7a85
Matthias Wimmer <m@tthias.eu>
2007-02-11 13:11:02 +0000

    Removed non-GPL files / new SHA1 implementation

--- jabberd/lib/jabberdlib.h
+++ jabberd/lib/jabberdlib.h
@@ -61,18 +61,122 @@
 #define INCL_LIB_H
 
 #ifdef __cplusplus
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdexcept>
+
+namespace xmppd {
+
+    /**
+     * generic base class for a hash function
+     */
+    class hash {
+	public:
+	    virtual void update(const std::string& data) =0;
+	    virtual std::vector<uint8_t> final() =0;
+	    std::string final_hex();
+    };
+
+    /**
+     * the SHA-1 hashing algorithm
+     */
+    class sha1 : public hash {
+	public:
+	    /**
+	     * construct a SHA-1 hashing instance
+	     */
+	    sha1();
+
+	    /**
+	     * add data to what the hash should be calculated for
+	     *
+	     * @param data the data that should get added
+	     */
+	    void update(const std::string& data);
+
+	    /**
+	     * add data to what the hash should be calculated for
+	     *
+	     * @param data the data that should get added
+	     */
+	    void update(const std::vector<uint8_t> data);
+
+	    /**
+	     * signal that all data has been added and request the hash
+	     *
+	     * @note use final_hex() to do the same but get a string result (hex version of the result)
+	     *
+	     * @return the hash value (binary)
+	     */
+	    std::vector<uint8_t> final();
+	private:
+	    /**
+	     * if set to true, the hash has been padded and no more data can be added
+	     */
+	    bool padded;
+
+	    /**
+	     * temporarily storage for blocks that have not yet been completed
+	     */
+	    std::vector<uint8_t> current_block;
+
+	    /**
+	     * W[0] to W[79] as defined in the SHA-1 standard
+	     */
+	    std::vector<uint32_t> W;
+
+	    /**
+	     * which byte of a block we are currently adding
+	     *
+	     * W_pos because this defines where in W where are adding the byte
+	     */
+	    unsigned W_pos;
+
+	    /**
+	     * H0 to H4 as defined in the SHA-1 standard
+	     */
+	    std::vector<uint32_t> H;
+
+	    /**
+	     * do the hashing calculations on a complete block, that is now in W[]
+	     */
+	    void hash_block();
+
+	    /**
+	     * the function S^n as defined in the SHA-1 standard
+	     */
+	    inline static uint32_t circular_shift(uint32_t X, int n);
+
+	    /**
+	     * the function f(t;B,C,D) for 0 <= t <= 19 as defined in the SHA-1 standard
+	     */
+	    inline static uint32_t f_0_19(uint32_t B, uint32_t C, uint32_t D);
+
+	    /**
+	     * the function f(t;B,C,D) for 20 <= t <= 39 as defined in the SHA-1 standard
+	     */
+	    inline static uint32_t f_20_39(uint32_t B, uint32_t C, uint32_t D);
+
+	    /**
+	     * the function f(t;B,C,D) for 40 <= t <= 59 as defined in the SHA-1 standard
+	     */
+	    inline static uint32_t f_40_59(uint32_t B, uint32_t C, uint32_t D);
+
+	    /**
+	     * the function f(t;B,C,D) for 60 <= t <= 79 as defined in the SHA-1 standard
+	     */
+	    inline static uint32_t f_60_79(uint32_t B, uint32_t C, uint32_t D);
+
+	    /**
+	     * the length of the message (l in the SHA-1 standard as well)
+	     */
+	    uint64_t l;
+    };
+}
+
 extern "C" {
-#endif
-
-
-#ifndef HAVE_SNPRINTF
-extern int ap_snprintf(char *, size_t, const char *, ...);
-#define snprintf ap_snprintf
-#endif
-
-#ifndef HAVE_VSNPRINTF
-extern int ap_vsnprintf(char *, size_t, const char *, va_list ap);
-#define vsnprintf ap_vsnprintf
 #endif
 
 #define ZONE zonestr(__FILE__,__LINE__)
@@ -206,8 +310,7 @@ typedef unsigned int uint32;
 
 char *shahash(char *str);	/* NOT THREAD SAFE */
 void shahash_r(const char* str, char hashbuf[41]); /* USE ME */
-
-int strprintsha(char *dest, int *hashval);
+void shaBlock(unsigned char *dataIn, int len, unsigned char hashout[20]);
 
 /* --------------------------------------------------------- */
 /*                                                           */
