f4a036140d648d7ede32c0cef4e9b99f49416404
Matthias Wimmer <m@tthias.eu>
2007-02-12 00:43:39 +0000

    License switch from JOSL to GPL.

--- jabberd14/jabberd/subjectAltName_asn1_tab.c
+++ jabberd14/jabberd/subjectAltName_asn1_tab.c
@@ -1,8 +1,29 @@
 /*
+ * Copyrights
+ *
  * generated from subjectAltName.asn using asn1Parser
  *
  * subjectAltName.asn is taken from GnuTLS and is licenced
- * under the terms of the GPL.
+ * under the terms of the LGPL.
+ * 
+ *
+ * This file is part of jabberd14.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
  */
 
 #ifdef HAVE_CONFIG_H
