6923e94a00770498047df9437f0df2a82233b301
mawis <mawis@localhost>
2007-07-18 22:40:59 +0000

    Fix by Martin Matuska for FreeBSD and maybe Linux as well (const part of the patch)

--- jabberd/lib/jutil.cc
+++ jabberd/lib/jutil.cc
@@ -261,7 +261,7 @@ char *jutil_timestamp_ms(char buffer[25]) {
     struct tm *new_time = NULL;
 
     gettimeofday(&tv, &tz);
-    new_time = gmtime(&(tv.tv_sec));
+    new_time = gmtime(static_cast<time_t*>(&(tv.tv_sec)));
     snprintf(buffer, sizeof(char[25]), "%d-%02d-%02dT%02d:%02d:%02d.%03dZ", 1900+new_time->tm_year,
 	     new_time->tm_mon+1, new_time->tm_mday, new_time->tm_hour,
 	     new_time->tm_min, new_time->tm_sec, tv.tv_usec/1000);
--- jabberd/log.cc
+++ jabberd/log.cc
@@ -282,7 +282,7 @@ void log_alert(const char *host, const char *msgfmt, ...) {
  * @param action action that is logged (e.g. a failed auth)
  * @param msgfmt printf()-like format string, parameters are following
  */
-void log_generic(const char *logtype, const char *id, const char *type, const char *action, const char *msgfmt, ...) {
+void log_generic(char const* logtype, const char *id, const char *type, const char *action, const char *msgfmt, ...) {
     va_list ap;
     char logmsg[512] = "";
     xmlnode log;
@@ -327,7 +327,7 @@ void log_generic(const char *logtype, const char *id, const char *type, const ch
  * @param action action that is logged (e.g. a failed auth)
  * @param msgfmt printf()-like format string, parameters are following
  */
-void log_record(const char *id, const char *type, const char *action, const char *msgfmt, ...) {
+void log_record(char const* id, const char *type, const char *action, const char *msgfmt, ...) {
     va_list ap;
     char logmsg[512] = "";
 
