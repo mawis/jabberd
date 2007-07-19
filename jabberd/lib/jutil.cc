905aede9cd14329f2d83ea0ecb433dd7bc76b407
mawis <mawis@localhost>
2007-07-19 23:47:28 +0000

    fix for the case that tv.tv_sec is not defined as time_t - seems to be the case on FreeBSD up to version 6.2

--- jabberd/lib/jutil.cc
+++ jabberd/lib/jutil.cc
@@ -257,11 +257,13 @@ char *jutil_timestamp(void) {
  */
 char *jutil_timestamp_ms(char buffer[25]) {
     struct timeval tv;
+    time_t secpart_of_tv;
     struct timezone tz;
     struct tm *new_time = NULL;
 
     gettimeofday(&tv, &tz);
-    new_time = gmtime(static_cast<time_t*>(&(tv.tv_sec)));
+    secpart_of_tv = static_cast<time_t>(tv.tv_sec);
+    new_time = gmtime(&secpart_of_tv);
     snprintf(buffer, sizeof(char[25]), "%d-%02d-%02dT%02d:%02d:%02d.%03dZ", 1900+new_time->tm_year,
 	     new_time->tm_mon+1, new_time->tm_mday, new_time->tm_hour,
 	     new_time->tm_min, new_time->tm_sec, tv.tv_usec/1000);
