9fcdf27be345a47719a0f2356017194af6ceb1b1
Matthias Wimmer <m@tthias.eu>
2019-09-08 16:31:00 +0200

    Allow tests to run.
    Not the most clean way to list the dependencies, but fine for getting it
    running. The dependencies are hopefully removed soon anyway.

--- jabberd14/jabberdlib/build.rs
+++ jabberd14/jabberdlib/build.rs
@@ -7,6 +7,11 @@ fn main() -> Result<()> {
 
     println!("cargo:rustc-link-search=native={}", clib.display());
     println!("cargo:rustc-link-lib=static=jabberdclib");
-
+    println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=idn");
+    println!("cargo:rustc-link-lib=glibmm-2.4");
+    println!("cargo:rustc-link-lib=gobject-2.0");
+    println!("cargo:rustc-link-lib=glib-2.0");
+    println!("cargo:rustc-link-lib=sigc-2.0");
     Ok(())
 }
